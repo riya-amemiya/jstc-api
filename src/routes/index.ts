73ee295f9275e7a0dd817f69a423097a23b15ead
Riya <hiyoriorria@gmail.com>
2021-02-09 20:48:43 +0900

    first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,64 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import { python, ruby } from "@jstc/core"
+const app = express();
+app.use( helmet() );
+app.use( cors() );
+// ルーティングする
+const router = express.Router();
+
+// routerにルーティングの動作を記述する
+router.get( '/python', ( req, res ) =>
+{
+    ( async () =>
+    {
+        if ( typeof req.query.code === "string" )
+        {
+            const acorn = await import( "acorn" )
+            const code: any = acorn.parse( req.query.code, {
+                ecmaVersion: "latest",
+                allowAwaitOutsideFunction: true,
+                allowImportExportEverywhere: true,
+                allowReserved: true
+            } )
+            res.status( 200 ).send( { code: python( code, "python" ).code } );
+        }
+    } )()
+} );
+router.get( '/ruby', ( req, res ) =>
+{
+    ( async () =>
+    {
+        if ( typeof req.query.code === "string" )
+        {
+            const acorn = await import( "acorn" )
+            const code: any = acorn.parse( req.query.code, {
+                ecmaVersion: "latest",
+                allowAwaitOutsideFunction: true,
+                allowImportExportEverywhere: true,
+                allowReserved: true
+            } )
+            res.status( 200 ).send( { code: ruby( code, "ruby" ).code } );
+        }
+    } )()
+} );
+
+// -------------------------------------------------
+//  以下、何のルーティングにもマッチしないorエラー
+// -------------------------------------------------
+
+// いずれのルーティングにもマッチしない(==NOT FOUND)
+app.use( ( req, res ) =>
+{
+    res.status( 404 );
+    res.render( 'error', {
+        param: {
+            status: 404,
+            message: 'not found'
+        },
+    } );
+} );
+
+//routerをモジュールとして扱う準備
+export { router }
